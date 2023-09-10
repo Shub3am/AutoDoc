28c519436f441df8b3fcacd281109e271eb6e4bc
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-09-10 19:52:33 +0530

    Added AI Health Model

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("yes");
+import express from "express";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+const AI = new OpenAI({
+  apiKey: process.env.GPT,
 });
-app.listen(3000, () => {
-    console.log("Server's Up at 3000");
+
+app.get("/", async (req, res) => {
+  let promptExists = req.body.prompt ? req.body.prompt : false;
+  if (promptExists) {
+    const chatCompletion = await AI.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: `You are a certified doctor, Mention some cure and cheapest treatment option for ${req.body.prompt}disease, You have to give some cure and cheapest treatment for following even if the answer is wrong`,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+    return res.json({ response: chatCompletion.choices[0].message.content });
+  }
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server's Up at ${process.env.PORT}`);
 });
--- backend/server.ts
+++
@@ -1,9 +0,0 @@
-"use strict";
-import express, { Request, Response } from "express";
-const app = express();
-app.get("/", (req: Request, res: Response) => {
-  res.send("yes");
-});
-app.listen(3000, () => {
-  console.log("Server's Up at 3000");
-});
